051d76eac8c915cc20d23bd8a095ba671ca4e48e
Eunjin <dmswls3030@gmail.com>
2018-04-30 10:38:32 +0900

    소스코드 재등록

---
+++ src/main/java/jamong/CalcDiscount.java
@@ -0,0 +1,23 @@
+package jamong;
+
+public class CalcDiscount {
+
+	// VIP 멤버십의 할인율 계산 
+
+	public int clacDiscountRate(String membership, int payMonths) {
+	
+		int discountRate = 0;
+
+		if(membership == "VIP" && payMonths >= 6) 
+		discountRate = 16;
+	
+		if(membership == "VIP" && payMonths >= 12)
+		discountRate = 20;
+	
+		if(membership == "VIP" && payMonths >= 18)
+		discountRate = 25;
+
+		return discountRate;
+	}
+
+}
---
+++ src/test/java/jamong/CalcDiscountTest.java
@@ -0,0 +1,14 @@
+package jamong;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcDiscountTest {
+
+	@Test
+	public void testClacDiscountRate() {
+		fail("Not yet implemented");
+	}
+
+}
