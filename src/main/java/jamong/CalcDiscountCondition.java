8c9fc3bbf70163633ec7226ffe15809c0ef3b71d
handongjoon <handongjoon@DESKTOP-TU2K18M>
2018-05-08 19:26:35 +0900

    EclEmma Branch 커버리지 확인

---
+++ src/main/java/jamong/CalcDiscountCondition.java
@@ -0,0 +1,45 @@
+package jamong;
+
+public class CalcDiscountCondition {
+
+	// VIP 멤버십의 할인율 계산 
+
+	public int clacDiscountRate(String membership, int payMonths) {
+	
+		int discountRate = 0;
+
+		if(membership.equals("VIP") == true && payMonths >= 6)
+		{	
+
+			discountRate = 16;
+		}
+		
+		
+/*		if(membership.equals("VIP") == true)
+		{	
+			if(payMonths >= 6)
+			{
+
+				discountRate = 16;
+			}
+		}
+		*/
+		this.result(membership, payMonths);
+		/*System.out.println("in if: " + (membership.equals("VIP") == true) + (payMonths >= 6));
+		System.out.println("out if: " + membership + payMonths);
+	*/
+			
+/*		if(membership == "VIP" && payMonths >= 12)
+		discountRate = 20;
+	
+		if(membership == "VIP" && payMonths >= 18)
+		discountRate = 25;*/
+
+		return discountRate;
+	}
+	
+	public void result(String membership, int payMonths) {
+		System.out.println("in if: " + (membership.equals("VIP") == true) + (payMonths >= 6));
+		//System.out.println("out if: " + membership + payMonths);
+	}
+}
---
+++ src/test/java/jamong/CalcDiscountConditionTest.java
@@ -0,0 +1,38 @@
+package jamong;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcDiscountConditionTest {
+
+	@Test
+	public void testVIPCondition1True() { //TT
+		CalcDiscountCondition calcVIP = new CalcDiscountCondition();
+		int result = calcVIP.clacDiscountRate("VIP", 8);
+		assertEquals(16, result);
+	}
+	
+	@Test
+	public void testVIPCondition1False() { //TF
+		CalcDiscountCondition calcVIP = new CalcDiscountCondition();
+		int result = calcVIP.clacDiscountRate("VIP", 0);
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void testVIPCondition1False2() { //FT
+		CalcDiscountCondition calcVIP = new CalcDiscountCondition();
+		int result = calcVIP.clacDiscountRate("VVIP", 8);
+		assertEquals(0, result);
+	}
+	
+	@Test
+	public void testVIPCondition1False3() { //FF
+		CalcDiscountCondition calcVIP = new CalcDiscountCondition();
+		int result = calcVIP.clacDiscountRate("VVIP", 0);
+		assertEquals(0, result);
+	}
+
+
+}
